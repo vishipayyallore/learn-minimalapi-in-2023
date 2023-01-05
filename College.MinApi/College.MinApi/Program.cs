428087a77e5121c31a4a146ca49700a243babdea
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-05 20:42:59 +0530

    Update Program.cs

--- College.MinApi/College.MinApi/Program.cs
+++ College.MinApi/College.MinApi/Program.cs
@@ -1,6 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => SendDefaultOutput());
+
+static object SendDefaultOutput()
+{
+    return new
+    {
+        Success = true,
+        Message = "Success",
+        Data = new
+        {
+            Content = "Welcome to Minimal API"
+        }
+    };
+}
 
 app.Run();
