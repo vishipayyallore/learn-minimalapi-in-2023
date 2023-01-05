0dabb2d3937f0dc4a82d9fd96312adf1c96ae36b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-05 20:51:38 +0530

    Update Program.cs

--- College.MinApi/College.MinApi/Program.cs
+++ College.MinApi/College.MinApi/Program.cs
@@ -1,9 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => SendDefaultOutput());
-
-static object SendDefaultOutput()
+static object GenerateDefaultOutput(string content)
 {
     return new
     {
@@ -11,9 +8,30 @@ static object SendDefaultOutput()
         Message = "Success",
         Data = new
         {
-            Content = "Welcome to Minimal API"
+            Content = content
         }
     };
 }
+
+static object SendDefaultApiEndpointOutput()
+{
+    return GenerateDefaultOutput("Welcome to Minimal API Endpoint");
+}
+
+static object SendDefaultApiEndpointV1Output()
+{
+    return GenerateDefaultOutput("Welcome to Minimal API Endpoint V1");
+}
+
+var app = builder.Build();
+
+app.MapGet("/", () =>
+{
+    return GenerateDefaultOutput("Hello Minimal API !!");
+});
+
+app.MapGet("/api", SendDefaultApiEndpointOutput);
+
+app.MapGet("/api/v1", () => SendDefaultApiEndpointV1Output());
 
 app.Run();
