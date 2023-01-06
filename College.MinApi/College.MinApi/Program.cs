7f1ab28745a5fb1340fac508e2eeeb298c94d5ab
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-06 12:13:14 +0530

    Response Dto

--- College.MinApi/College.MinApi/Program.cs
+++ College.MinApi/College.MinApi/Program.cs
@@ -1,26 +1,26 @@
+using College.MinApi.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 
-static object GenerateDefaultOutput(string content)
+static CollegeApiResponseDto<T> GenerateCollegeApiResponse<T>(T? data = default, string message = "Success"
+    , bool success = true)
 {
-    return new
+    return new CollegeApiResponseDto<T>
     {
-        Success = true,
-        Message = "Success",
-        Data = new
-        {
-            Content = content
-        }
+        Success = success,
+        Message = message,
+        Data = data
     };
 }
 
 static object SendDefaultApiEndpointOutput()
 {
-    return GenerateDefaultOutput("Welcome to Minimal API Endpoint");
+    return GenerateCollegeApiResponse<string>("Welcome to Minimal API Endpoint");
 }
 
 static object SendDefaultApiEndpointV1Output()
 {
-    return GenerateDefaultOutput("Welcome to Minimal API Endpoint V1");
+    return GenerateCollegeApiResponse<string>("Welcome to Minimal API Endpoint V1");
 }
 
 var app = builder.Build();
@@ -29,7 +29,7 @@ app.MapGet("/", () => "Hello Minimal API World !!");
 
 app.MapGet("/hw", () =>
 {
-    return GenerateDefaultOutput("Hello Minimal API World !!");
+    return GenerateCollegeApiResponse<string>("Hello Minimal API World !!");
 });
 
 app.MapGet("/api", SendDefaultApiEndpointOutput);
