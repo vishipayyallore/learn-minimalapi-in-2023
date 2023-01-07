54e78bf359bfbe46bd4a8571135ae7ac6fec4955
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-07 14:52:31 +0530

    Code Refactoring

--- College.MinApi/College.MinApi/Common/Constants.cs
+++ College.MinApi/College.MinApi/Common/Constants.cs
@@ -3,6 +3,17 @@
 
     public static class Constants
     {
+        public static class HelloWorldEndpoints
+        {
+            public static string Root { get; } = "/";
+
+            public static string HelloWorld { get; } = "/hw";
+
+            public static string Api { get; } = "/api";
+
+            public static string ApiV1 { get; } = "/api/v1";
+        }
+
         public static class CoursesEndpoints
         {
             public static string Root { get; } = "/api/courses";
--- College.MinApi/College.MinApi/Program.cs
+++ College.MinApi/College.MinApi/Program.cs
@@ -1,10 +1,11 @@
-using College.MinApi.Common;
 using College.MinApi.Entities;
 using College.MinApi.Extensions;
 using College.MinApi.Helpers;
 using College.MinApi.Interfaces;
 using College.MinApi.Persistance;
 using College.MinApi.Repositories;
+
+using static College.MinApi.Common.Constants;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,38 +16,38 @@ builder.Services.AddApplicationServices();
 var app = builder.Build();
 
 # region Root & Hello World Endpoints
-app.MapGet("/", () => "Hello Minimal API World !!");
+app.MapGet(HelloWorldEndpoints.Root, () => "Hello Minimal API World from Root !!");
 
-app.MapGet("/hw", () =>
+app.MapGet(HelloWorldEndpoints.HelloWorld, () =>
 {
-    return CollegeApiResponse.GenerateCollegeApiResponse<string>("Hello Minimal API World !!");
+    return CollegeApiResponse.GenerateCollegeApiResponse("Hello Minimal API World from /hw !!");
 });
 
-app.MapGet("/api", DefaultApiResponse.SendDefaultApiEndpointOutput);
+app.MapGet(HelloWorldEndpoints.Api, DefaultApiResponse.SendDefaultApiEndpointOutput);
 
-app.MapGet("/api/v1", () => DefaultApiResponse.SendDefaultApiEndpointV1Output());
+app.MapGet(HelloWorldEndpoints.ApiV1, () => DefaultApiResponse.SendDefaultApiEndpointV1Output());
 #endregion
 
 #region Courses Endpoints
-app.MapGet(Constants.CoursesEndpoints.Root, async (ICoursesRepository coursesRepository) =>
+app.MapGet(CoursesEndpoints.Root, async (ICoursesRepository coursesRepository) =>
 {
     var courses = await coursesRepository.GetAllCourses();
 
     return Results.Ok(courses);
 });
 
-app.MapPost(Constants.CoursesEndpoints.Root, async (CollegeDbContext collegeDbContext, Course course) =>
+app.MapPost(CoursesEndpoints.Root, async (CollegeDbContext collegeDbContext, Course course) =>
 {
     collegeDbContext.Courses.Add(course);
 
     await collegeDbContext.SaveChangesAsync();
 
-    return Results.Created($"{Constants.CoursesEndpoints.Root}/{course.Id}", course);
+    return Results.Created($"{CoursesEndpoints.Root}/{course.Id}", course);
 });
 #endregion
 
 #region Students Endpoints
-app.MapGet(Constants.StudentsEndpoints.Root, StudentsRepository.GetAllStudents);
+app.MapGet(StudentsEndpoints.Root, StudentsRepository.GetAllStudents);
 #endregion
 
 app.Run();
