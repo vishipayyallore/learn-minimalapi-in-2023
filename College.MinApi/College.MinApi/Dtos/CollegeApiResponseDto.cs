accb6504a52d4e48b3ad35fc2f47a4b3efe27fed
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-07 08:17:14 +0530

    Swamy/06dec2023 work (#2)
    * Folder
    
    * Update Program.cs
    
    * Response Dto
    
    * Get All Students

---
+++ College.MinApi/College.MinApi/Dtos/CollegeApiResponseDto.cs
@@ -0,0 +1,13 @@
+﻿namespace College.MinApi.Dtos
+{
+
+    public class CollegeApiResponseDto<T>
+    {
+        public bool Success { get; set; }
+
+        public string? Message { get; set; }
+
+        public T? Data { get; set; }
+    }
+
+}
--- College.MinApi/College.MinApi/Program.cs
+++ College.MinApi/College.MinApi/Program.cs
@@ -1,37 +1,54 @@
+using College.MinApi.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 
-static object GenerateDefaultOutput(string content)
+static CollegeApiResponseDto<T> GenerateCollegeApiResponse<T>(T? data = default, string message = "Success"
+    , bool success = true)
 {
-    return new
+    return new CollegeApiResponseDto<T>
     {
-        Success = true,
-        Message = "Success",
-        Data = new
-        {
-            Content = content
-        }
+        Success = success,
+        Message = message,
+        Data = data
     };
 }
 
-static object SendDefaultApiEndpointOutput()
+static CollegeApiResponseDto<string> SendDefaultApiEndpointOutput()
 {
-    return GenerateDefaultOutput("Welcome to Minimal API Endpoint");
+    return GenerateCollegeApiResponse<string>("Welcome to Minimal API Endpoint");
 }
 
-static object SendDefaultApiEndpointV1Output()
+static CollegeApiResponseDto<string> SendDefaultApiEndpointV1Output()
 {
-    return GenerateDefaultOutput("Welcome to Minimal API Endpoint V1");
+    return GenerateCollegeApiResponse<string>("Welcome to Minimal API Endpoint V1");
 }
 
 var app = builder.Build();
 
-app.MapGet("/", () =>
+app.MapGet("/", () => "Hello Minimal API World !!");
+
+app.MapGet("/hw", () =>
 {
-    return GenerateDefaultOutput("Hello Minimal API !!");
+    return GenerateCollegeApiResponse<string>("Hello Minimal API World !!");
 });
 
 app.MapGet("/api", SendDefaultApiEndpointOutput);
 
 app.MapGet("/api/v1", () => SendDefaultApiEndpointV1Output());
+
+app.MapGet("/api/students", GetAllStudents);
+
+static CollegeApiResponseDto<IEnumerable<StudentDto>> GetAllStudents()
+{
+    IList<StudentDto> students = new List<StudentDto>()
+    {
+        new() { Name = "Sri Varu"},
+        new() { Name = "Manpreet Sing"},
+        new() { Name = "Scott Rudy"},
+        new() { Name = "Mohd Azim"}
+    };
+
+    return GenerateCollegeApiResponse<IEnumerable<StudentDto>>(students);
+}
 
 app.Run();
