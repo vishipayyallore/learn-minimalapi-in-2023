c2d2917a5a742ca132724339d44d31bf9bfdf7f5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-13 16:02:55 +0530

    Entites

---
+++ SchoolApp/School.Data/Entities/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace School.Data.Entities
+{
+
+    public class Student : BaseEntity
+    {
+        public string? RollNumber { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public DateTime DateofBirth { get; set; }
+
+        public string? Picture { get; set; }
+    }
+
+}
