caaf0164c50ac1ea620e942d1f7c082d8ffb49bb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-13 13:54:51 +0530

    Update Program.cs

--- College.MinApi/College.MinApi/Program.cs
+++ College.MinApi/College.MinApi/Program.cs
@@ -31,35 +31,35 @@ app.MapGet(HelloWorldEndpoints.ApiV1, () => DefaultApiResponse.SendDefaultApiEnd
 app.MapGet(CoursesEndpoints.Root, async ([FromServices] ICoursesBusiness coursesBusiness) =>
 {
     return Results.Ok(await coursesBusiness.GetAllCourses());
-});
+}).WithName("GetAllCourses");
 
 app.MapPost(CoursesEndpoints.Root, async ([FromBody] CourseDto courseDto, [FromServices] ICoursesBusiness coursesBusiness) =>
 {
     (string courseId, ApiResponseDto<CourseDto> apiResponse) = await coursesBusiness.AddCourse(courseDto);
 
     return Results.Created($"{CoursesEndpoints.Root}/{courseId}", apiResponse);
-});
+}).WithName("AddCourse");
 
 app.MapGet(CoursesEndpoints.ActionById, async (Guid Id, [FromServices] ICoursesBusiness coursesBusiness) =>
 {
     var apiResponse = await coursesBusiness.GetCourseById(Id);
 
     return apiResponse.Data is null ? Results.NotFound() : Results.Ok(apiResponse);
-});
+}).WithName("GetCourseById");
 
 app.MapPut(CoursesEndpoints.ActionById, async (Guid Id, [FromBody] CourseDto courseDto, [FromServices] ICoursesBusiness coursesBusiness) =>
 {
     var apiResponse = await coursesBusiness.UpdateCourseById(Id, courseDto);
 
     return apiResponse.Data is null ? Results.NotFound() : Results.Ok(apiResponse);
-});
+}).WithName("UpdateCourseById");
 
 app.MapDelete(CoursesEndpoints.ActionById, async (Guid Id, [FromServices] ICoursesBusiness coursesBusiness) =>
 {
     var apiResponse = await coursesBusiness.DeleteCourseById(Id);
 
     return apiResponse.Data is null ? Results.NotFound() : Results.Ok(apiResponse);
-});
+}).WithName("DeleteCourseById");
 #endregion
 
 #region Students Endpoints
