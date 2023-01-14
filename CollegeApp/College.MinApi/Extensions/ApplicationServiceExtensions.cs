d3b8a4cf182393b242728cc19eb7be6352d6f681
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-14 12:25:12 +0530

    Created Configuration Project and moved its code

--- CollegeApp/College.MinApi/Extensions/ApplicationServiceExtensions.cs
+++ CollegeApp/College.MinApi/Extensions/ApplicationServiceExtensions.cs
@@ -13,14 +13,14 @@ namespace College.MinApi.Extensions
 
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
-            services.AddDbContext<CollegeDbContext>(options =>
+            _ = services.AddDbContext<CollegeDbContext>(options =>
                 options.UseInMemoryDatabase("CollegeDatabase"));
 
-            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
+            _ = services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
 
-            services.AddScoped<ICoursesBusiness, CoursesBusiness>();
+            _ = services.AddScoped<ICoursesBusiness, CoursesBusiness>();
 
-            services.AddScoped<ICoursesRepository, CoursesRepository>();
+            _ = services.AddScoped<ICoursesRepository, CoursesRepository>();
 
             return services;
         }
