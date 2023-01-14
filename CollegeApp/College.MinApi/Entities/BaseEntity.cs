820bf88bdf22abf58e8976fd47f2a04ee06f7099
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-14 08:59:25 +0530

    Swamy/13jan2023 work (#12)
    * Update CoursesBusiness.cs
    
    * Update CoursesRepository.cs
    
    * Update CoursesRepository.cs
    
    * Moved Update into Business Layer
    
    * Delete By Id
    
    * Verified GetAll, GetById, UpdateById, DeleteById
    
    * Initial Version
    
    * Update Program.cs
    
    * Using Records for Dtos
    
    * Entites
    
    * Using Base Entity
    
    * Rename

---
+++ CollegeApp/College.MinApi/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿namespace College.MinApi.Entities
+{
+
+    public class BaseEntity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+        public string CreatedBy { get; set; } = "Admin";
+
+        public DateTime ModifiedDate { get; set; } = DateTime.UtcNow;
+
+        public string ModifiedBy { get; set; } = "Admin";
+    }
+
+}
