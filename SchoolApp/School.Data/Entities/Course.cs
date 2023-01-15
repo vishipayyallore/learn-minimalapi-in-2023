e20ab949f413a57c8412d530872087252154c7ff
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-15 11:23:20 +0530

    Entities

---
+++ SchoolApp/School.Data/Entities/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School.Data.Entities
+{
+    
+    public class Course : BaseEntity
+    {
+        public string? Title { get; set; }
+
+        public int Credits { get; set; }
+    }
+
+}
