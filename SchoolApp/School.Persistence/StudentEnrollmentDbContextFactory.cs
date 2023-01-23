4968afee4a66a4cbacaa4c47f1403687f00ea58c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-23 15:49:50 +0530

    Package Updates

---
+++ SchoolApp/School.Persistence/StudentEnrollmentDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+
+namespace School.Persistence
+{
+    public class StudentEnrollmentDbContextFactory : IDesignTimeDbContextFactory<SchoolAppDbContext>
+    {
+        public SchoolAppDbContext CreateDbContext(string[] args)
+        {
+            // Get environment
+            //string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            // Build config
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            // Get connection string
+            var optionsBuilder = new DbContextOptionsBuilder<SchoolAppDbContext>();
+            var connectionString = config.GetConnectionString("SchoolAppDbConnection");
+            optionsBuilder.UseSqlServer(connectionString);
+            return new SchoolAppDbContext(optionsBuilder.Options);
+        }
+    }
+
+}
