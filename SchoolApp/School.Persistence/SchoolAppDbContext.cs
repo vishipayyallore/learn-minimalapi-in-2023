b93b9b429693ea8d3350146f6c9010ad50f20cb5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-09 13:00:41 +0530

    Code Refactoring

--- SchoolApp/School.Persistence/SchoolAppDbContext.cs
+++ SchoolApp/School.Persistence/SchoolAppDbContext.cs
@@ -1,32 +1,28 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using School.Data.Entities;
-using School.Data.SeedData;
 
-namespace School.Persistence
+namespace School.Persistence;
+
+public class SchoolAppDbContext : IdentityDbContext
 {
-
-    public class SchoolAppDbContext : IdentityDbContext
+    public SchoolAppDbContext(DbContextOptions<SchoolAppDbContext> options) : base(options)
     {
-        public SchoolAppDbContext(DbContextOptions<SchoolAppDbContext> options) : base(options)
-        {
-        }
+    }
 
-        public DbSet<Course> Courses => Set<Course>();
+    public DbSet<Course> Courses => Set<Course>();
 
-        public DbSet<Student> Students => Set<Student>();
+    public DbSet<Student> Students => Set<Student>();
 
-        public DbSet<Enrollment> Enrollments => Set<Enrollment>();
+    public DbSet<Enrollment> Enrollments => Set<Enrollment>();
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
 
-            builder.ApplyConfiguration(new CourseData());
+        builder.ApplyConfiguration(new CourseData());
 
-            builder.ApplyConfiguration(new UserRoleData());
-        }
-
+        builder.ApplyConfiguration(new UserRoleData());
     }
 
 }
--- SchoolApp/School.Persistence/SeedData/CourseData.cs
+++ SchoolApp/School.Persistence/SeedData/CourseData.cs
@@ -2,37 +2,35 @@
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using School.Data.Entities;
 
-namespace School.Data.SeedData
+namespace School.Persistence.SeedData;
+
+internal class CourseData : IEntityTypeConfiguration<Course>
 {
-    internal class CourseData : IEntityTypeConfiguration<Course>
+
+    public void Configure(EntityTypeBuilder<Course> builder)
     {
-
-        public void Configure(EntityTypeBuilder<Course> builder)
-        {
-            _ = builder.HasData(
-                new
-                {
-                    Id = Guid.NewGuid(),
-                    Title = "Minimal API Development",
-                    Credits = 3,
-                    CreatedDate = DateTime.UtcNow,
-                    CreatedBy = "Admin",
-                    ModifiedDate = DateTime.UtcNow,
-                    ModifiedBy = "Admin"
-                },
-                new
-                {
-                    Id = Guid.NewGuid(),
-                    Title = "Ultimate API Development",
-                    Credits = 5,
-                    CreatedDate = DateTime.UtcNow,
-                    CreatedBy = "Admin",
-                    ModifiedDate = DateTime.UtcNow,
-                    ModifiedBy = "Admin"
-                }
-            );
-        }
-
+        _ = builder.HasData(
+            new
+            {
+                Id = Guid.NewGuid(),
+                Title = "Minimal API Development",
+                Credits = 3,
+                CreatedDate = DateTime.UtcNow,
+                CreatedBy = "Admin",
+                ModifiedDate = DateTime.UtcNow,
+                ModifiedBy = "Admin"
+            },
+            new
+            {
+                Id = Guid.NewGuid(),
+                Title = "Ultimate API Development",
+                Credits = 5,
+                CreatedDate = DateTime.UtcNow,
+                CreatedBy = "Admin",
+                ModifiedDate = DateTime.UtcNow,
+                ModifiedBy = "Admin"
+            }
+        );
     }
 
 }
