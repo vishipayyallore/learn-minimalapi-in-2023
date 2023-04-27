3b7a888a2310e070a0e05d988a97e421b1c38c49
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-04-28 00:41:12 +0530

    First Kafka Producer Demo (#27)
    * Initial Version
    
    * Update First-KafkaDemo.csproj
    
    * Few Changes
    
    * Update Program.cs
    
    * Changes

---
+++ First-KafkaDemo/First-KafkaDemo/Program.cs
@@ -0,0 +1,58 @@
+using Confluent.Kafka;
+using First_KafkaDemo.Interfaces;
+using First_KafkaDemo.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var producerConfig = new ProducerConfig();
+
+var ProducerTopic = builder.Configuration.GetValue<string>("Topic");
+builder.Configuration.Bind("ProducerConfig", producerConfig);
+builder.Services.AddSingleton<ProducerConfig>(producerConfig);
+// builder.Services.AddSingleton(ProducerTopic);
+
+builder.Services.AddScoped<IProducerService, ProducerService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.MapGet("/", () => "Welcome to Kafka Producer!");
+
+app.MapPost("/send-message-to-kafka", async (HttpContext http) =>
+{
+    var messageToKafka = "";
+
+    using (StreamReader sr = new(http.Request.Body))
+    {
+        messageToKafka = await sr.ReadToEndAsync();
+    }
+
+    if (string.IsNullOrWhiteSpace(messageToKafka))
+    {
+        throw new InvalidDataException("Must have a message body");
+    }
+
+    var producerService = http.RequestServices.GetRequiredService<IProducerService>();
+
+    await producerService.SetTopic(ProducerTopic!);
+
+    try
+    {
+        await producerService.Send(messageToKafka);
+    }
+    catch (Exception ex)
+    {
+        await http.Response.BodyWriter.WriteAsync(System.Text.Encoding.ASCII.GetBytes(ex.Message));
+
+        http.Response.StatusCode = 500;
+    }
+
+    http.Response.StatusCode = 200;
+});
+
+
+app.Run();
---
+++ First-KafkaDemo/First-KafkaDemo/Services/ProducerService.cs
@@ -0,0 +1,50 @@
+﻿using Confluent.Kafka;
+using First_KafkaDemo.Interfaces;
+
+namespace First_KafkaDemo.Services;
+
+public class ProducerService : IProducerService
+{
+    private readonly ProducerConfig _config = default!;
+    private readonly ILogger<ProducerService> _logger = default!;
+    private readonly IProducer<int, string> _producer = default!;
+    private readonly ProducerBuilder<int, string> _builder = default!;
+
+    public string? Topic { get; private set; }
+
+    public ProducerService(ProducerConfig config, ILogger<ProducerService> logger)
+    {
+        _config = config ?? throw new ArgumentNullException(nameof(config));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+        Topic = string.Empty;
+
+        _builder = new ProducerBuilder<int, string>(_config);
+        _producer = _builder.Build();
+    }
+
+    public async Task Send(string message)
+    {
+        await Task.Run(() =>
+        {
+            var messagePacket = new Message<int, string>() { Key = 1, Value = message };
+
+            _producer.ProduceAsync(Topic, messagePacket, CancellationToken.None);
+        });
+    }
+
+    public async Task SetTopic(string topicName)
+    {
+        await Task.Run(() => { Topic = topicName; });
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
