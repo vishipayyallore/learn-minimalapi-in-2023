6ecf9e7dbf6279c1e89bec1761449a859bc614a2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-08 19:31:03 +0530

    Migrations

--- StudentEnrollment/src/StudentEnrollment.Data/Migrations/20230708140004_DataSeedingV1.Designer.cs
+++ StudentEnrollment/src/StudentEnrollment.Data/Migrations/20230708140004_DataSeedingV1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StudentEnrollment.Data.Persistence;
 
@@ -11,9 +12,11 @@ using StudentEnrollment.Data.Persistence;
 namespace StudentEnrollment.Data.Migrations
 {
     [DbContext(typeof(StudentEnrollmentDbContext))]
-    partial class StudentEnrollmentDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230708140004_DataSeedingV1")]
+    partial class DataSeedingV1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ StudentEnrollment/src/StudentEnrollment.Data/Migrations/20230708140004_DataSeedingV1.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StudentEnrollment.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeedingV1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "13a5014e-85e0-4f38-ad8f-2a9f25df5b47");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3a48f41a-21d2-420a-aedf-b42e47a3127b");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "34f068f1-bb9f-4282-aee8-6e13dbd021e6", null, "Administrator", "ADMINISTRATOR" },
+                    { "4e7cc2f4-ac6e-4eba-b355-ef1fab152e30", null, "User", "USER" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "ModifiedDate" },
+                values: new object[] { new DateTime(2023, 7, 8, 19, 30, 4, 48, DateTimeKind.Local).AddTicks(4700), new DateTime(2023, 7, 8, 19, 30, 4, 48, DateTimeKind.Local).AddTicks(4714) });
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "ModifiedDate" },
+                values: new object[] { new DateTime(2023, 7, 8, 19, 30, 4, 48, DateTimeKind.Local).AddTicks(4717), new DateTime(2023, 7, 8, 19, 30, 4, 48, DateTimeKind.Local).AddTicks(4717) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "34f068f1-bb9f-4282-aee8-6e13dbd021e6");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4e7cc2f4-ac6e-4eba-b355-ef1fab152e30");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "13a5014e-85e0-4f38-ad8f-2a9f25df5b47", null, "Administrator", "ADMINISTRATOR" },
+                    { "3a48f41a-21d2-420a-aedf-b42e47a3127b", null, "User", "USER" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "ModifiedDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "ModifiedDate" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+        }
+    }
+}
