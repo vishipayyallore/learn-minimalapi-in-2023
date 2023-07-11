30d8604222bfe926fa38fc345b093e61fad90827
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-11 13:45:05 +0530

    Swamy/09jul student enrollment day 2 (#33)
    * Update Program.cs
    
    * Update Program.cs
    
    * Update Program.cs
    
    * StudentEnrollmentDbContextFactory
    
    * Code Refactoring
    
    * Scaffolded Items
    
    * Update Program.cs
    
    * Few Changes
    
    * dotnet ef

--- CollegeApp/College.Persistence/CollegeDbContext.cs
+++ CollegeApp/College.Persistence/CollegeDbContext.cs
@@ -1,16 +1,24 @@
 ﻿using College.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using StudentEnrollment.Data.Configurations;
 
 namespace College.MinApi.Persistance
 {
 
     public class CollegeDbContext : DbContext
     {
+        public DbSet<Course> Courses => Set<Course>();
+
         public CollegeDbContext(DbContextOptions options) : base(options)
         {
         }
 
-        public DbSet<Course> Courses => Set<Course>();
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            _ = builder.ApplyConfiguration(new CourseSeedData());
+        }
     }
 
 }
