aefefae5fdead1498dca9fbefa666502f5e9a15f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-15 17:13:05 +0530

    Package Updates, and DTOs

---
+++ StudentEnrollment/src/StudentEnrollment.Data/Dtos/Course/CreateCourseDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentEnrollment.Data.Dtos.Course;
+
+public record CreateCourseDto
+{
+    [Required]
+    public string? Title { get; set; }
+
+    [Required]
+    public int Credits { get; set; }
+}
