1e6700279a50551ff67d50d86f8d363602bab364
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-17 14:53:58 +0530

    Student Dtos

---
+++ StudentEnrollment/src/StudentEnrollment.Data/Dtos/Student/CreateStudentDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentEnrollment.Data.Dtos.Student;
+
+public class CreateStudentDto
+{
+    [Required]
+    public string? FirstName { get; set; }
+
+    [Required]
+    public string? LastName { get; set; }
+
+    [Required]
+    public DateTime? DateOfBirth { get; set; }
+
+    [Required]
+    public string? IdNumber { get; set; }
+
+    [Required]
+    public byte[]? ProfilePicture { get; set; }
+}
+
---
+++ StudentEnrollment/src/StudentEnrollment.Data/Dtos/Student/StudentDto.cs
@@ -0,0 +1,6 @@
+﻿namespace StudentEnrollment.Data.Dtos.Student;
+
+public class StudentDto : CreateStudentDto
+{
+    public int Id { get; set; }
+}
