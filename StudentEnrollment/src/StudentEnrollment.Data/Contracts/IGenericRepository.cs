bfacc579bd117399b2f78b2bec754b1065dbb714
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-22 00:01:34 +0530

    Swamy/21jul endpoints with dtos (#41)
    * Initial Version
    
    * Update GenericRepository.cs

---
+++ StudentEnrollment/src/StudentEnrollment.Data/Contracts/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using StudentEnrollment.Data.Entities;
+
+namespace StudentEnrollment.Data.Contracts;
+
+public interface IGenericRepository<TEntity> where TEntity : BaseEntity
+{
+    Task<TEntity> GetAsync(int? id);
+
+    Task<List<TEntity>> GetAllAsync();
+
+    Task<TEntity> AddAsync(TEntity entity);
+
+    Task<bool> DeleteAsync(int id);
+
+    Task UpdateAsync(TEntity entity);
+
+    Task<bool> Exists(int id);
+}
