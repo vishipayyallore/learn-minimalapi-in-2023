c6122ccc0ef9d0d439012c1fb643590547d7f913
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-21 22:34:47 +0530

    Update GenericRepository.cs

--- StudentEnrollment/src/StudentEnrollment.Data/Repositories/GenericRepository.cs
+++ StudentEnrollment/src/StudentEnrollment.Data/Repositories/GenericRepository.cs
@@ -1,12 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using StudentEnrollment.Data.Contracts;
+using StudentEnrollment.Data.Entities;
+using StudentEnrollment.Data.Persistence;
 
-namespace StudentEnrollment.Data.Repositories
+namespace StudentEnrollment.Data.Repositories;
+
+public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : BaseEntity
 {
-    internal class GenericRepository
+    protected readonly StudentEnrollmentDbContext _db;
+
+    public GenericRepository(StudentEnrollmentDbContext db)
     {
+        this._db = db;
+    }
+
+    public async Task<TEntity> AddAsync(TEntity entity)
+    {
+        await _db.AddAsync(entity);
+        await _db.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var entity = await GetAsync(id);
+        _db.Set<TEntity>().Remove(entity);
+        return await _db.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> Exists(int id)
+    {
+        return await _db.Set<TEntity>().AnyAsync(q => q.Id == id);
+    }
+
+    public async Task<List<TEntity>> GetAllAsync()
+    {
+        return await _db.Set<TEntity>().ToListAsync();
+    }
+
+    public async Task<TEntity> GetAsync(int? id)
+    {
+        var result = await _db.Set<TEntity>().FindAsync(id);
+        return result;
+    }
+
+    public async Task UpdateAsync(TEntity entity)
+    {
+        _db.Update(entity);
+        await _db.SaveChangesAsync();
     }
 }
