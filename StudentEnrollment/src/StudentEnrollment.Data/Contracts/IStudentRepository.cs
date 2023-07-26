88e8e86ef7677b47be3be3cf0eb192c6a624c257
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-26 23:31:48 +0530

    Student Repository

---
+++ StudentEnrollment/src/StudentEnrollment.Data/Contracts/IStudentRepository.cs
@@ -0,0 +1,8 @@
+﻿using StudentEnrollment.Data.Entities;
+
+namespace StudentEnrollment.Data.Contracts;
+
+public interface IStudentRepository : IGenericRepository<Student>
+{
+    Task<Student?> GetStudentDetails(int studentId);
+}
