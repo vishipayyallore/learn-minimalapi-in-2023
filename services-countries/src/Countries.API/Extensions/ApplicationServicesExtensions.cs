5c1c9ec37c4c6af81b74f08a4d00caef9c02f24c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-05 15:50:21 +0530

    Changes

--- services-countries/src/Countries.API/Extensions/ApplicationServicesExtensions.cs
+++ services-countries/src/Countries.API/Extensions/ApplicationServicesExtensions.cs
@@ -1,9 +1,17 @@
-﻿namespace Countries.API.Extensions;
+﻿using Countries.ApplicationCore.Interfaces;
+using Countries.Business;
+using Countries.Repositories;
+
+namespace Countries.API.Extensions;
 
 public static class ApplicationServicesExtensions
 {
     public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
     {
+        _ = services.AddScoped<ICountriesBusiness, CountriesBusiness>();
+
+        _ = services.AddScoped<ICountriesRepository, CountriesRepository>();
+
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         _ = services.AddEndpointsApiExplorer();
         _ = services.AddSwaggerGen();
