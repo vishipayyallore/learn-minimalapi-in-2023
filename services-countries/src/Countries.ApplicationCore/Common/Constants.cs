8a06fcfbe019ee5f2b5aa9c990595773ce479602
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-05 14:59:08 +0530

    Changes

---
+++ services-countries/src/Countries.ApplicationCore/Common/Constants.cs
@@ -0,0 +1,24 @@
+﻿namespace Countries.ApplicationCore.Common;
+
+public static class Constants
+{
+    public static class WelcomeRoutes
+    {
+        public static string Root => "/";
+
+        public static string Api => "/api";
+    }
+
+    public static class CountriesRoutes
+    {
+        public static string Prefix { get; } = "/api/countries";
+
+        public static string Root => "/";
+    }
+
+    public static class SqlDatabaseConnectionStringName
+    {
+        public static string Name { get; } = "CountriesDbConnection";
+    }
+
+}
