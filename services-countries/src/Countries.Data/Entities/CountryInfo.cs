701ba5b14c6bed5230ad659e4dc934e6f5e57f5b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-05 15:19:19 +0530

    Changes

---
+++ services-countries/src/Countries.Data/Entities/CountryInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace Countries.Data.Entities;
+
+public class CountryInfo
+{
+    public int CountryId { get; set; }
+
+    public string? CountryName { get; set; }
+
+    public string? CapitalState { get; set; }
+
+    public string? NationalBird { get; set; }
+
+    public int CountryPopulation { get; set; }
+}
