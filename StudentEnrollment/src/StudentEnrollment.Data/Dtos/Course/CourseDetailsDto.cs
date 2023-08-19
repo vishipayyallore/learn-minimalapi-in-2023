ab65699c6ce7fae65695a213cf37d4cebff6c72e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-19 21:16:16 +0530

    Changes

---
+++ StudentEnrollment/src/StudentEnrollment.Data/Dtos/Course/CourseDetailsDto.cs
@@ -0,0 +1,14 @@
+﻿using StudentEnrollment.Data.Dtos.Student;
+
+namespace StudentEnrollment.Data.Dtos.Course;
+
+public class CourseDetailsDto
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public int Credits { get; set; }
+
+    public List<StudentDto> Students { get; set; } = new();
+}
