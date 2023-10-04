0496006185453555119d891c5f34277d849533be
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-04 20:11:13 +0530

    Added Core and Data

---
+++ School-App/src/School.API/Endpoints/CourseEndpoints.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using School.Api.ApplicationCore.Interfaces;
+using School.Api.Data.Dtos;
+using School.Api.Data.Entities;
+
+namespace School.Api.Endpoints;
+
+public static class CourseEndpoints
+{
+
+    public static void MapCourseEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup(CoursesRoutes.Prefix).WithTags(nameof(Course));
+
+        _ = group.MapGet(CoursesRoutes.Root, async ([FromServices] ICoursesBusiness coursesBusiness) =>
+        {
+            return Results.Ok(await coursesBusiness.GetAllCourses());
+        })
+          .AllowAnonymous()
+          .WithName("GetAllCourses")
+          .Produces<ApiResponseDto<IReadOnlyCollection<CourseDto>>>(StatusCodes.Status200OK)
+          .ProducesProblem(StatusCodes.Status500InternalServerError)
+          .WithOpenApi();
+
+    }
+
+}
---
+++ School-App/src/School.API/Endpoints/UserEndpoints.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using School.Api.Data.Dtos;
+
+namespace School.Api.Endpoints;
+
+public static class UserEndpoints
+{
+
+    public static void MapUserEndpoints(this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup(UsersRoutes.Prefix).WithTags("Users");
+
+        _ = group.MapGet(UsersRoutes.ActionById, ([FromRoute] string id, [FromQuery] string name) =>
+        {
+            return ApiResponseDto<dynamic>.Create(new
+            {
+                UserId = id,
+                Message = $"Hello {name}, Welcome to Minimal API World !!"
+            });
+        })
+          .AllowAnonymous()
+          .WithName("GetUserById")
+          .Produces<ApiResponseDto<dynamic>>(StatusCodes.Status200OK)
+          .ProducesProblem(StatusCodes.Status500InternalServerError)
+          .WithOpenApi();
+
+        _ = group.MapPost(UsersRoutes.Root, ([FromBody] PersonDto person) =>
+        {
+            return ApiResponseDto<dynamic>.Create(new
+            {
+                UserId = person.Id,
+                UserName = person.Name,
+            });
+        })
+          .WithName("AddNewUser")
+          .Produces<ApiResponseDto<dynamic>>(StatusCodes.Status201Created)
+          .ProducesProblem(StatusCodes.Status500InternalServerError)
+          .WithOpenApi();
+
+    }
+
+}
